f77794d29d919c054b7498d3333e3aecfe09d2e9
Johannes Matt <johannes.matt@quantics.io>
2022-04-18 14:49:09 +0200

    Add main application

---
+++ src/main/java/io/jomatt/multitenant/sample/MultiTenantApplication.java
@@ -0,0 +1,13 @@
+package io.jomatt.multitenant.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiTenantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiTenantApplication.class, args);
+    }
+
+}
